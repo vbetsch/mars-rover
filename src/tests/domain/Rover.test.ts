0ff1e55a296472c54f4b6159d2b5125a603be2d6
vbetsch <vic.betsch@gmail.com>
2025-06-25 22:18:57 +0200

    refactor: Rename test

--- src/tests/domain/Rover.test.ts
+++ src/tests/domain/Rover.test.ts
@@ -2,7 +2,7 @@ import { Point } from '@app/domain/Point';
 import { Rover } from '@app/domain/Rover';
 
 describe('Rover', () => {
-  it('Rover - should have a starting point', () => {
+  it('Rover - should be at its starting point', () => {
     const startingPoint: Point.Class = new Point.Class({ x: 0, y: 0 });
     const rover: Rover.Class = new Rover.Class({ point: startingPoint });
 
