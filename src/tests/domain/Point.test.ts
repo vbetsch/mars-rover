7a6a3bdd55e0da6af0ba11f00ac4f5487462500d
vbetsch <vic.betsch@gmail.com>
2025-06-25 22:00:18 +0200

    test: Create 'Point - x and y should be positive' test

--- src/tests/domain/Point.test.ts
+++ src/tests/domain/Point.test.ts
@@ -8,4 +8,21 @@ describe('Point', () => {
     expect(point.x).toBe(params.x);
     expect(point.y).toBe(params.y);
   });
+  it('Point - x and y should be positive', () => {
+    expect(() => new Point.Class({ x: -1, y: 0 })).toThrow(
+      'x cannot be negative'
+    );
+    expect(() => new Point.Class({ x: -1, y: 1 })).toThrow(
+      'x cannot be negative'
+    );
+    expect(() => new Point.Class({ x: 0, y: -1 })).toThrow(
+      'y cannot be negative'
+    );
+    expect(() => new Point.Class({ x: 1, y: -1 })).toThrow(
+      'y cannot be negative'
+    );
+    expect(() => new Point.Class({ x: -1, y: -1 })).toThrow(
+      'x cannot be negative'
+    );
+  });
 });
