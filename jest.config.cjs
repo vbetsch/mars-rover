70ee9434d2190a2f669694189d06556310d05bde
vbetsch <vic.betsch@gmail.com>
2025-06-25 21:16:54 +0200

    refactor: New architecture

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+  testMatch: ['**/src/tests/**/*.test.ts'],
   transform: {
     ...tsJestTransformCfg,
   },
