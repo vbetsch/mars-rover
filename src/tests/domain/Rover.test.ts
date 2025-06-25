0c041e0c7916f4e67dd9761d646ef99ca442a9fc
vbetsch <vic.betsch@gmail.com>
2025-06-25 22:24:08 +0200

    test: Upgrade Rover test

--- src/tests/domain/Rover.test.ts
+++ src/tests/domain/Rover.test.ts
@@ -2,10 +2,15 @@ import { Point } from '@app/domain/Point';
 import { Rover } from '@app/domain/Rover';
 
 describe('Rover', () => {
-  it('Rover - should be at its starting point', () => {
-    const startingPoint: Point.Class = new Point.Class({ x: 0, y: 0 });
-    const rover: Rover.Class = new Rover.Class({ point: startingPoint });
+  it('Rover - should have a starting point and a direction', () => {
+    const point: Point.Class = new Point.Class({ x: 0, y: 0 });
+    const direction: DirectionEnum = DirectionEnum.EAST;
+    const rover: Rover.Class = new Rover.Class({
+      startingPoint: point,
+      direction: direction,
+    });
 
-    expect(rover.position).toBe(startingPoint);
+    expect(rover.position).toBe(point);
+    expect(rover.direction).toBe(direction);
   });
 });
