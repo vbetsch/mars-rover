e3762d53a4436f71597e54329ee6f7947af0f6a4
vbetsch <vic.betsch@gmail.com>
2025-06-25 20:03:14 +0200

    test: Create 'Point - should have x and y' test

---
+++ tests/point.test.ts
@@ -0,0 +1,10 @@
+describe('Point', () => {
+    it('Point - should have x and y', () => {
+        const x: number = 0;
+        const y: number = 0;
+        const point: Point = new Point(x, y);
+
+        expect(point.x).toBe(x);
+        expect(point.y).toBe(y);
+    });
+})
