c6367c58a27d249fd8d9aaad076db0cf73bb1615
vbetsch <vic.betsch@gmail.com>
2025-06-25 21:09:01 +0200

    refactor: Integrate namespaces

--- src/point.ts
+++ src/point.ts
@@ -1,22 +1,24 @@
-type PointParams = {
-  readonly x: number;
-  readonly y: number;
-};
+export namespace Point {
+  export type Type = {
+    readonly x: number;
+    readonly y: number;
+  };
 
-export class Point {
-  private readonly _x: number;
-  private readonly _y: number;
+  export class Class {
+    private readonly _x: number;
+    private readonly _y: number;
 
-  public constructor(params: PointParams) {
-    this._x = params.x;
-    this._y = params.y;
-  }
+    public constructor(params: Type) {
+      this._x = params.x;
+      this._y = params.y;
+    }
 
-  public get x(): number {
-    return this._x;
-  }
+    public get x(): number {
+      return this._x;
+    }
 
-  public get y(): number {
-    return this._y;
+    public get y(): number {
+      return this._y;
+    }
   }
 }
