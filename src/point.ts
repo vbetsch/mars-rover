91efa726c818a1cc328c311ff7d75a8402375162
vbetsch <vic.betsch@gmail.com>
2025-06-25 20:26:01 +0200

    refactor: Rewrite Point class

--- src/point.ts
+++ src/point.ts
@@ -1,9 +1,25 @@
 export class Point {
-  x: number;
-  y: number;
+  private _x: number;
+  private _y: number;
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  public constructor(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
+
+  public get x(): number {
+    return this._x;
+  }
+
+  public set x(x: number) {
+    this._x = x;
+  }
+
+  public get y(): number {
+    return this._y;
+  }
+
+  public set y(y: number) {
+    this._y = y;
   }
 }
