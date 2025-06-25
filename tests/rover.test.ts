5c61ea94901bd096d8baaf8daf08d3a525b34b4a
vbetsch <vic.betsch@gmail.com>
2025-06-25 20:00:32 +0200

    Revert "test: Create 'should have a starting point' test"
    This reverts commit 26350b15266eabdf7cdca955421e279c45ecf5a9.

--- tests/rover.test.ts
+++
@@ -1,8 +0,0 @@
-describe("Rover", () => {
-    it('should have a starting point', () => {
-        const startingPoint: Point = new Point();
-        const rover: Rover = new Rover(startingPoint);
-
-        expect(rover.startingPoint).toBe(startingPoint);
-    });
-})
