4bb8f6e96ad30e79988b787b19e65ff1a7419094
vbetsch <vic.betsch@gmail.com>
2025-06-25 20:19:52 +0200

    tech: Improve linter and formatter configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,58 @@
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: [
+export default tseslint.config(
+  [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+      ignores: [
         'node_modules',
         'dist',
         'jest.config.cjs',
     ],
-  },
+    },
+    {
+      files: ['**/*.ts'],
+      languageOptions: {
+        parserOptions: {
+          project: './tsconfig.json',
+        },
+      },
+      plugins: {
+        prettier: eslintPluginPrettier,
+      },
+      rules: {
+        'prettier/prettier': ['error', { semi: true }],
+        semi: ['error', 'always'],
+        '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'explicit' }],
+        'no-var': 'error',
+        'prefer-const': ['error', { destructuring: 'all' }],
+        '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: false }],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/prefer-readonly': 'warn',
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'classProperty',
+            modifiers: ['private'],
+            format: ['camelCase'],
+            leadingUnderscore: 'require',
+          },
+        ],
+        'no-undefined': 'warn',
+        '@typescript-eslint/adjacent-overload-signatures': 'warn',
+      },
+    },
+  ],
   {
+    // Nécessaire pour activer le parsing avec type info
     files: ['**/*.ts'],
-    plugins: {
-      prettier: eslintPluginPrettier,
+    languageOptions: {
+      parser: tseslint.parser,
     },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
-];
+  }
+);
