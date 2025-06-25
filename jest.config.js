ed75f6c115d812a640694bf0367c81cb5b45a85c
vbetsch <vic.betsch@gmail.com>
2025-06-25 19:15:07 +0200

    tech: Update jest config

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,10 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
+    preset: 'ts-jest',
+    testEnvironment: "node",
+    testMatch: ['**/tests/**/*.test.ts'],
+    transform: {
     ...tsJestTransformCfg,
-  },
+    },
 };
