2d840136ec530847ae152dfb0339d7218c72c8b0
vbetsch <vic.betsch@gmail.com>
2025-06-26 23:06:35 +0200

    test: Create 'Compass - north should have mustIncrementY' test

--- src/tests/domain/compass.test.ts
+++ src/tests/domain/compass.test.ts
@@ -1,7 +1,13 @@
+import { CardinalPoint } from '@app/domain/cardinal-point';
 import { Compass } from '@app/domain/compass';
 
 describe('Compass', () => {
   it('Compass - should have cardinal points', () => {
     expect(Compass.Instance.cardinalPoints).toStrictEqual(['N', 'E', 'S', 'W']);
+  });
+  it('Compass - north should have mustIncrementY', () => {
+    const cardinalPoint: CardinalPoint.Class =
+      Compass.Instance.getCardinalPoint('N');
+    expect(cardinalPoint.mustIncrementY).toBe(true);
   });
 });
