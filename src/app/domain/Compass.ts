36824b46208ae0645ee803dabeebe4752fe67def
vbetsch <vic.betsch@gmail.com>
2025-06-26 22:07:14 +0200

    Revert "feat: Create get cardinal points methods in Compass"
    This reverts commit c69de7b4c0e8938806095a21dd1b745b92bb470a.

--- src/app/domain/Compass.ts
+++ src/app/domain/Compass.ts
@@ -8,21 +8,5 @@ export namespace Compass {
       new CardinalPoint.Class({ value: CardinalPoint.Enum.SOUTH }),
       new CardinalPoint.Class({ value: CardinalPoint.Enum.WEST }),
     ];
-
-    public getNorth(): CardinalPoint.Enum {
-      return this._cardinalPoints[0].value;
-    }
-
-    public getEast(): CardinalPoint.Enum {
-      return this._cardinalPoints[1].value;
-    }
-
-    public getSouth(): CardinalPoint.Enum {
-      return this._cardinalPoints[2].value;
-    }
-
-    public getWest(): CardinalPoint.Enum {
-      return this._cardinalPoints[3].value;
-    }
   }
 }
