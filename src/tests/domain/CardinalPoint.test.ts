62bdd7b97f6c174f1a8381e7a09ea2c7ddef8d8c
vbetsch <vic.betsch@gmail.com>
2025-06-26 22:20:43 +0200

    test: Create 'CardinalPoint - should have a value' test

---
+++ src/tests/domain/CardinalPoint.test.ts
@@ -0,0 +1,9 @@
+describe('CardinalPoint', () => {
+  it('CardinalPoint - should have a value', async () => {
+    const cardinalPoint: CardinalPoint.Class = new CardinalPoint.Class({
+      value: 'NORTH',
+    });
+
+    expect(cardinalPoint.value).toStrictEqual('NORTH');
+  });
+});
