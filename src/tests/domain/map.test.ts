ab1f6dd7a4e1400084feff7b8df6a21e8be2300b
vbetsch <vic.betsch@gmail.com>
2025-06-27 19:08:53 +0200

    test: Create 'Map - should have the size set' test

---
+++ src/tests/domain/map.test.ts
@@ -0,0 +1,13 @@
+describe('Map', () => {
+  it('Map - should have the size set', () => {
+    const height: number = 5;
+    const width: number = 4;
+    const map: Map.Class = new Map.Class({
+      height: height,
+      width: width,
+    });
+
+    expect(map.height).toBe(height);
+    expect(map.width).toBe(width);
+  });
+});
