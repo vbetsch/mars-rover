22695f2ed8399b02314ad09b2219587e21fd715e
vbetsch <vic.betsch@gmail.com>
2025-06-27 19:00:30 +0200

    feat: Create a program to move Rover

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
+import { Rover } from '@app/domain/rover';
+import { Point } from '@app/domain/point';
+import { Compass } from '@app/domain/compass';
+import { CardinalPoint } from '@app/domain/cardinal-point';
+
+function _displayDirection(direction: CardinalPoint.Class): string {
+  switch (direction) {
+    case Compass.Instance.getCardinalPoint(Compass.CardinalPointEnum.NORTH):
+      return '⬆️';
+    case Compass.Instance.getCardinalPoint(Compass.CardinalPointEnum.EAST):
+      return '➡️';
+    case Compass.Instance.getCardinalPoint(Compass.CardinalPointEnum.SOUTH):
+      return '⬇️';
+    case Compass.Instance.getCardinalPoint(Compass.CardinalPointEnum.WEST):
+      return '⬅️';
+    default:
+      throw new Error(`Unknown direction: ${direction}`);
+  }
+}
+
+function displayRover(rover: Rover.Class): string {
+  return `direction: ${_displayDirection(rover.direction)}  |  position: ${rover.position.x},${rover.position.y}`;
+}
+
 console.log('Hello World!');
+
+console.log('Creating a Rover...');
+const rover: Rover.Class = new Rover.Class({
+  startingPoint: new Point.Class({ x: 0, y: 0 }),
+  direction: Compass.Instance.getCardinalPoint(Compass.CardinalPointEnum.EAST),
+});
+console.log('\nYour Rover is created !');
+console.log(displayRover(rover));
+
+console.log('\nYour Rover is moving forward...');
+rover.moveForward();
+console.log(displayRover(rover));
+
+console.log('\nYour Rover is turning left...');
+rover.turnLeft();
+console.log(displayRover(rover));
+
+console.log('\nYour Rover is moving forward...');
+rover.moveForward();
+console.log(displayRover(rover));
+
+console.log('\nYour Rover is turning right...');
+rover.turnRight();
+console.log(displayRover(rover));
+
+console.log('\nYour Rover is moving forward...');
+rover.moveForward();
+console.log(displayRover(rover));
