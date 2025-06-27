7a80f5ad3c20a9d49035bf2999b8548b88ffa0b9
vbetsch <vic.betsch@gmail.com>
2025-06-27 19:22:24 +0200

    feat: Create matrix

--- src/app/domain/map.ts
+++ src/app/domain/map.ts
@@ -7,10 +7,24 @@ export namespace Map {
   export class Class {
     private readonly _height: number;
     private readonly _width: number;
+    private _matrix: string[][] = [];
+
+    private _computeMatrix(): void {
+      const matrix: string[][] = [];
+      for (let y = 0; y < this.height; y++) {
+        const line: string[] = [];
+        for (let x = 0; x < this.width; x++) {
+          line.push('X');
+        }
+        matrix.push(line);
+      }
+      this._matrix = matrix;
+    }
 
     public constructor(params: Params) {
       this._height = params.height;
       this._width = params.width;
+      this._computeMatrix();
     }
 
     public get height(): number {
@@ -19,6 +33,10 @@ export namespace Map {
 
     public get width(): number {
       return this._width;
+    }
+
+    public get matrix(): string[][] {
+      return this._matrix;
     }
   }
 }
