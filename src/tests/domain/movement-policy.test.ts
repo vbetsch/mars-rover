c4cf104651f2cfbd00a9d411d25e08c115af8ab5
vbetsch <vic.betsch@gmail.com>
2025-06-30 19:43:19 +0200

    test: Add tests about isMoveAllowed method

--- src/tests/domain/movement-policy.test.ts
+++ src/tests/domain/movement-policy.test.ts
@@ -1,5 +1,6 @@
 import { Map } from '@app/domain/map';
 import { MovementPolicy } from '@app/domain/movement-policy';
+import { Point } from '@app/domain/point';
 
 describe('MovementPolicy', () => {
   it('MovementPolicy - should have a map', () => {
@@ -12,5 +13,56 @@ describe('MovementPolicy', () => {
     });
 
     expect(policy.map).toStrictEqual(map);
+  });
+  it('MovementPolicy - should not allow to move', () => {
+    const policy: MovementPolicy.Class = new MovementPolicy.Class({
+      map: new Map.Class({
+        height: 3,
+        width: 3,
+      }),
+    });
+
+    expect(
+      policy.isMoveAllowed(
+        new Point.Class({
+          x: 5,
+          y: 5,
+        })
+      )
+    ).toBe(false);
+  });
+  it('MovementPolicy - should allow to move', () => {
+    const policy: MovementPolicy.Class = new MovementPolicy.Class({
+      map: new Map.Class({
+        height: 3,
+        width: 3,
+      }),
+    });
+
+    expect(
+      policy.isMoveAllowed(
+        new Point.Class({
+          x: 2,
+          y: 3,
+        })
+      )
+    ).toBe(true);
+  });
+  it('MovementPolicy - should allow to move', () => {
+    const policy: MovementPolicy.Class = new MovementPolicy.Class({
+      map: new Map.Class({
+        height: 3,
+        width: 3,
+      }),
+    });
+
+    expect(
+      policy.isMoveAllowed(
+        new Point.Class({
+          x: 3,
+          y: 2,
+        })
+      )
+    ).toBe(true);
   });
 });
