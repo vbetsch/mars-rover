7402da3847d78063c95175322e190481c9d8e865
vbetsch <vic.betsch@gmail.com>
2025-06-30 19:57:24 +0200

    test: Test all movement cases

--- src/tests/domain/movement-policy.test.ts
+++ src/tests/domain/movement-policy.test.ts
@@ -14,7 +14,7 @@ describe('MovementPolicy', () => {
 
     expect(policy.map).toStrictEqual(map);
   });
-  it('MovementPolicy - should not allow to move', () => {
+  it('MovementPolicy - should not allow to move - both exceed', () => {
     const policy: MovementPolicy.Class = new MovementPolicy.Class({
       map: new Map.Class({
         height: 3,
@@ -31,7 +31,58 @@ describe('MovementPolicy', () => {
       )
     ).toBe(false);
   });
-  it('MovementPolicy - should allow to move', () => {
+  it('MovementPolicy - should not allow to move - only x exceed', () => {
+    const policy: MovementPolicy.Class = new MovementPolicy.Class({
+      map: new Map.Class({
+        height: 3,
+        width: 3,
+      }),
+    });
+
+    expect(
+      policy.isMoveAllowed(
+        new Point.Class({
+          x: 4,
+          y: 2,
+        })
+      )
+    ).toBe(false);
+  });
+  it('MovementPolicy - should not allow to move - only y exceed', () => {
+    const policy: MovementPolicy.Class = new MovementPolicy.Class({
+      map: new Map.Class({
+        height: 3,
+        width: 3,
+      }),
+    });
+
+    expect(
+      policy.isMoveAllowed(
+        new Point.Class({
+          x: 2,
+          y: 4,
+        })
+      )
+    ).toBe(false);
+  });
+  it('MovementPolicy - should allow to move - both on the border', () => {
+    const policy: MovementPolicy.Class = new MovementPolicy.Class({
+      map: new Map.Class({
+        height: 3,
+        width: 3,
+      }),
+    });
+
+    expect(
+      policy.isMoveAllowed(
+        new Point.Class({
+          x: 3,
+          y: 3,
+        })
+      )
+    ).toBe(true);
+  });
+  it('MovementPolicy - should allow to move - y is on the border', () => {
     const policy: MovementPolicy.Class = new MovementPolicy.Class({
       map: new Map.Class({
         height: 3,
@@ -48,7 +99,7 @@ describe('MovementPolicy', () => {
       )
     ).toBe(true);
   });
-  it('MovementPolicy - should allow to move', () => {
+  it('MovementPolicy - should allow to move - x is on the border', () => {
     const policy: MovementPolicy.Class = new MovementPolicy.Class({
       map: new Map.Class({
         height: 3,
